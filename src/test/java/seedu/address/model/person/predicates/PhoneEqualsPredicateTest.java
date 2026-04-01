0f6de7040749c874118e30bc59a2638634b64afa
Tan Ze Jian <110755344+onetw0three@users.noreply.github.com>
2026-04-01 16:34:11 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/model/person/predicates/PhoneEqualsPredicateTest.java
+++ src/test/java/seedu/address/model/person/predicates/PhoneEqualsPredicateTest.java
@@ -3,7 +3,6 @@ package seedu.address.model.person.predicates;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.Assert.assertThrows;
 
 import java.util.Arrays;
 import java.util.List;
